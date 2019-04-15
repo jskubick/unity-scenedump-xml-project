6b00351b9b0a1dc765661ebdd754e18bc19492ab
jskubick <jeff-github@skubick.com>
2019-04-15 18:14:06 -0400

    Initial commit, still need to add editor extension itself (unity-scenedump-xml) as a submodule under Assets/Editor/

---
+++ Assets/Scripts/ExampleRenderableClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using pantherkitty.unity.scenedump;
+
+namespace pantherkitty.unity.scenedump.example {
+	public class ExampleRenderableClass {
+		public void dumpToXml(XmlElement e) {
+			XmlElement child = e.OwnerDocument.CreateElement(e.Prefix, "value", e.NamespaceURI);
+			e.AppendChild(child);
+			child.SetAttribute("name", "someValue");
+			child.SetAttribute("type", "String");
+			child.InnerText = ("meaningless value");
+
+
+			child = e.OwnerDocument.CreateElement(e.Prefix, "value", e.NamespaceURI);
+			e.AppendChild(child);
+			child.SetAttribute("name", "min");
+			child.SetAttribute("type", "System.Int32");
+			child.InnerText = ("17");
+
+			child = e.OwnerDocument.CreateElement(e.Prefix, "value", e.NamespaceURI);
+			e.AppendChild(child);
+			child.SetAttribute("name", "max");
+			child.SetAttribute("type", "System.Int32");
+			child.InnerText = "42";
+		}
+	}
+}
---
+++ Assets/Scripts/PointlessBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace pantherkitty.unity.scenedump.example {
+	public class PointlessBehavior : MonoBehaviour {
+		public bool isBooleanArg = true;
+
+		public ExampleRenderableClass someClassInstance = new ExampleRenderableClass();
+
+		public int intProperty { get; set; } = 42;
+		public int intMember = 69;
+		public int[][] complexIntArray;
+
+		public String stringProperty { get; set; } = "This is a string property";
+		public String stringMember = "This is a public string member";
+		public String[] simpleStringArray = new String[3] { "one", "two", "three" };
+		public String[,] twoDimStringArray = new String[,] { { "uno", "dos", "tres" }, { "eins", "zwei", "drei" } };
+
+		public Transform someOtherTransform;
+		public GameObject someGameObject;
+		public UnityEngine.Camera ourMainCamera;
+
+		public PointlessBehavior() {
+
+			complexIntArray = new int[5][];
+			for (int y = 0; y < 5; y++) {
+				int size = (y % 3) + 2;
+				complexIntArray[y] = new int[size];
+				for (int x = 0; x < size; x++) {
+					complexIntArray[y][x] = y * x;
+				}
+			}
+		}
+
+		// Start is called before the first frame update
+		void Start() {
+
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+		}
+	}
+}
